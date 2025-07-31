826bbdedddcff2a8b63ac95672e88977876660b9
Jhonod <akmjonod@gmail.com>
2025-07-31 09:44:49 +0700

    destroy resource automated terrformm

--- main.tf
+++ main.tf
@@ -11,28 +11,7 @@ resource "google_artifact_registry_repository" "registry" {
   docker_config {
     immutable_tags = false
   }
-}
 
-resource "google_cloud_run_service" "app" {
-  name     = "warung-kopi"
-  location = var.region
-  depends_on = [google_artifact_registry_repository.registry]
-
-  template {
-    spec {
-      containers {
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repository_id}"
-        ports {
-          container_port = 8080
-        }
-      }
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
 }
 
 resource "google_cloud_run_service_iam_member" "noauth" {
